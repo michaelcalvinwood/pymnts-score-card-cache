10be639f09b231d0e60b5e4891ca5512891abba9
Michael Wood <michaelwood33311@icloud.com>
2024-01-31 14:38:08 -0500

    added mysql connectivity

--- pymnts-score-card-cache.js
+++ pymnts-score-card-cache.js
@@ -1,5 +1,38 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const mysql = require('mysql2');
+
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
+const mysqlOptions = {
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+}
+
+const pool = mysql.createPool(mysqlOptions);
+
+const query = q => {
+  return new Promise((resolve, reject) => {
+    pool.query(q, function(err, rows, fields) {
+      console.error(err);
+      if (err) return resolve(false);
+      resolve(rows)
+    });
+  })
+}
+
+query('SHOW DATABASES')
+.then(response => console.log(response))
+.catch(err => console.error(err));
 
 const topLevelLinks = [
 "https://www.pymnts.com/mobile-order-ahead-rankings/",
@@ -91,4 +124,4 @@ const getAllPages = async () => {
     
 }
 
-getAllPages();
+//getAllPages();
